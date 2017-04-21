bd88aff697e57edbd490a8332ac1db9a7b44e6d6
Suman Daram <Suman.Daram@jda.com>
2017-04-21 12:48:58 +0530

    Suman| Initial commit

---
+++ app/src/main/java/com/jda/a1021597/myfirstapplication/MainActivity.java
@@ -0,0 +1,45 @@
+package com.jda.a1021597.myfirstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MyTags","OnCreate invoked");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MyTags","onStart invoked");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MyTags","onPause invoked");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MyTags","onResume invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MyTags","onStop invoked");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MyTags","onDestroy invoked");
+    }
+}
